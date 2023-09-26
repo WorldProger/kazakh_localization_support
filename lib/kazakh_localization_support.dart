ff0adc49b3705749cc8d563be68a104cfb223b92
marat <prog.freeman@gmail.com>
2023-09-26 12:16:04 +0500

    init

--- lib/kazakh_localization_support.dart
+++ lib/kazakh_localization_support.dart
@@ -5,4 +5,5 @@ export 'src/kz_cupertino_localizations.dart';
 // Support for android/windows
 export 'src/kz_material_localizations.dart';
 // Combination of kz cupertino and material localizations
+
 export 'src/kz_delegates.dart';
